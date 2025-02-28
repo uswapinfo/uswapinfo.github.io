98780ac7b3d2e876c7f82e885ddc548263fea933
uswap-app <132031468+uswap-app@users.noreply.github.com>
2025-02-28 14:15:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,8 +16,7 @@ $(window).bind("load", function () {
     ];
 
     var he_rpc_nodes = [
-        "https://engine.rishipanthee.com", 
-        "https://ha.herpc.dtools.dev", 
+        "https://enginerpc.com", 
         "https://api.hive-engine.com",
         "https://api.primersion.com",
         "https://herpc.actifit.io"
